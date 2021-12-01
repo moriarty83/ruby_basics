595708c855b402ea7ec4f4f89b72ab4979010c6c
moriarty83 <moriarty83@gmail.com>
2021-12-01 18:41:36 -0500

    array methods complete

--- basics.rb
+++ basics.rb
@@ -24,3 +24,37 @@ nums.unshift(5)
 
 puts "nums after unshift #{nums}\n"
 
+puts "nums length = #{nums.length}"
+
+puts "nums includes 8: #{nums.include?(8)}"
+
+puts "nums over 10: #{nums.find_all{|i| i > 10}}"
+
+puts "nums all > 0: #{nums.all?{ |i| i>0}}"
+
+puts "nums divisible by 8: #{nums.any?{|i| i>0 && i%8 == 0}}"
+
+puts "count of nums > 4: #{nums.count{|i| i>4}}"
+
+product = []
+nums.each_with_index do |ele,index|
+    
+    product.push(ele*index)
+end
+puts "product of index and element: #{product}"
+
+puts "num divisable by 7 & 5: #{nums.find{|i|i% 7 == 0 && i % 5 == 0}}"
+
+puts "Index of num divisable by 7 & 5: #{nums.find_index(nums.find{|i| i != 0 && i% 7 == 0 && i % 5 == 0})}"
+
+puts "first three nums: #{nums.first(3)}"
+
+puts "last five nums: #{nums.last(5)}"
+
+puts "groub by %3: #{nums.group_by{|i| i%3}}"
+
+puts "min max nums: #{nums.minmax()}"
+
+puts "rejected non multiples of 3: #{nums.reject{|i|i%3==0}}"
+
+puts "selected multiples of 5: #{nums.select{|i| i%5==0}}"
