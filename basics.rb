40529da3705292ad8f7f3e1975148e15530abcf0
moriarty83 <moriarty83@gmail.com>
2021-11-30 22:38:29 -0500

    first two commands

---
+++ basics.rb
@@ -0,0 +1,9 @@
+puts "hello world"
+
+adjective = "Big Bad"
+
+puts "Hello #{adjective} World"
+
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0, 35, 19, 66, 3, 22, 21, 4, 4, 4, 0, 1, 2, 3, 4, 12, 15]
+
+puts nums.uniq
