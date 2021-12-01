09633f818f7b539b38a3af71a18eae32d125492e
moriarty83 <moriarty83@gmail.com>
2021-12-01 09:18:45 -0500

    array methods

--- basics.rb
+++ basics.rb
@@ -6,4 +6,21 @@ puts "Hello #{adjective} World"
 
 nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0, 35, 19, 66, 3, 22, 21, 4, 4, 4, 0, 1, 2, 3, 4, 12, 15]
 
-puts nums.uniq
+puts "unique nums #{nums.uniq}\n\n"
+
+nums.push(5)
+
+puts "nums after push #{nums}\n\n"
+
+nums.pop()
+
+puts "nums after pop #{nums}\n\n"
+
+nums.shift
+
+puts "nums after shift #{nums}\n\n"
+
+nums.unshift(5)
+
+puts "nums after unshift #{nums}\n"
+
