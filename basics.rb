28ff71bb9a12a9e741fe0e50f2d552b749659966
moriarty83 <moriarty83@gmail.com>
2021-12-01 19:12:39 -0500

    methods

--- basics.rb
+++ basics.rb
@@ -4,6 +4,11 @@ adjective = "Big Bad"
 
 puts "Hello #{adjective} World"
 
+
+#############################
+## Nums Array and Enumerables
+#############################
+
 nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0, 35, 19, 66, 3, 22, 21, 4, 4, 4, 0, 1, 2, 3, 4, 12, 15]
 
 puts "unique nums #{nums.uniq}\n\n"
@@ -58,3 +63,38 @@ puts "min max nums: #{nums.minmax()}"
 puts "rejected non multiples of 3: #{nums.reject{|i|i%3==0}}"
 
 puts "selected multiples of 5: #{nums.select{|i| i%5==0}}"
+
+#############################
+## Color Array
+#############################
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+puts "random color: #{colors[rand(colors.length)]}"
+
+reverse_colors = colors.reverse.each{|i| i}
+puts "reverse colors: #{reverse_colors}"
+
+capital_colors = [] 
+colors.each{|i| capital_colors.push(i.capitalize)}
+puts "capital colors: #{capital_colors}"
+
+#############################
+## Methods
+#############################
+
+def find_area height, width
+    height*width
+end
+
+puts find_area(2, 4)
+
+nums_2 = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+def multiply_each_by_five arr
+    new_arr = []
+    arr.each{|i| new_arr.push(i*5)}
+    new_arr
+end
+
+puts "multiply_each_by_five: #{multiply_each_by_five nums_2}"
