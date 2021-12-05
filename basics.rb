ef0249aa473902b2b1aa369711dea21624baa8a2
moriarty83 <moriarty83@gmail.com>
2021-12-05 10:27:24 -0500

    captial colors using .map

--- basics.rb
+++ basics.rb
@@ -105,9 +105,9 @@ puts "reverse colors: #{reverse_colors}"
 
 # 3.
 # Question, is there a way to use .each to read directly into an array instead of using push? What's the better way to do this that I'm missing?
-capital_colors = [] 
-colors.each{|i| capital_colors.push(i.capitalize)}
-puts "capital colors: #{capital_colors}"
+# THANKS FOR THE ANSWER CHRIS!
+puts "Capital Colors #{colors.map{|i| i.capitalize}}"
+
 
 #############################
 ## Methods
@@ -232,6 +232,11 @@ p pandigital 12347865
 dickens = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way--in short, the period was so far like the present period that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only."
 
 def word_freq(words)
+    final_count = 0
+    word = ""
+    for word in words.split(" ")
+
+    end
     p words.split(" ")
 end
 
